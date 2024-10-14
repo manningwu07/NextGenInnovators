b29a1d53427f53ec97b14d1ed56eb73ec588f6ea
Dylan Price <dylankenjiprice@gmail.com>
2024-10-14 13:45:17 -0700

    stash

--- src/pages/non-profit-website.tsx
+++ src/pages/non-profit-website.tsx
@@ -6,8 +6,8 @@ import TeamMembers from "~/components/TeamMembers";
 import DonateModal from "~/components/Donate";
 import Link from "next/link";
 import type { Activity } from "~/types/event";
-import upcomingActivities from '~/changeContentHere/upcomingActivities.json';
-import pastActivities from '~/changeContentHere/pastActivities.json';
+import upcomingActivities from '~/changeContentHere/CurrentEvents.json';
+import pastActivities from '~/changeContentHere/PastEvents.json';
 
 
 // Use the type to assert the imported JSON data
